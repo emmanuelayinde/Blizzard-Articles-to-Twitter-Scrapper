290931fe6e935c1919778007d044638c7b0cdcf6
engr-solution <dah.solu.tion7@gmail.com>
2022-08-31 18:52:15 +0100

    inital commit

---
+++ utils/scrapper.py
@@ -0,0 +1,58 @@
+# IMPORT NECCESSARY LIB
+import os
+import time
+from utils.formatter import format_description_text
+from utils.tweet import tweet
+
+path = os.getcwd()
+default_media = path + '/blizzard.png'
+
+def blizzard_forum_scrapper(driver, WebDriverWait, By, EC):
+    driver.implicitly_wait(10)
+    driver.get('https://us.forums.blizzard.com/en/hearthstone/g/blizzard-tracker/activity/topics')
+
+    url = None 
+
+    all_bliss_articles = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "a.tracked-post.group-community-manager")))
+    top_ten_articles = all_bliss_articles[:10] 
+
+    for new_url in top_ten_articles:
+        tweeted = False
+        with open(path +"/data/tweeted_articles.txt") as f:
+            for line in f:
+                if line.strip() == new_url.get_attribute('href'):
+                    tweeted = True
+                    break
+        if tweeted:
+            continue  
+        else: 
+            with open(path +"/data/tweeted_articles.txt", 'a') as f:
+                f.write(new_url.get_attribute('href') + '\n')
+                url = new_url.get_attribute('href')
+            break
+
+    if url == None:
+        print('No new articles available at the moment')        
+    else:
+        scrape_articles(driver, WebDriverWait, By, EC, url)
+        driver.quit()
+        print('done..............')    
+
+
+def scrape_articles(driver, WebDriverWait, By, EC, url):
+    driver.get(url)
+    time.sleep(10)
+    title = driver.find_element(By.CSS_SELECTOR, "a.fancy-title").text
+
+    intro = '📢---Forum known issues update spotted---📢'
+    url = driver.current_url
+   
+    text = f"{intro}\n\n📺 {title}\n\nSource: {url}"
+
+    # UPLOAD TO TWITTER
+    tweet(text, media = default_media)
+    
+    time.sleep(5)
+    driver.quit()
+    
+    
---
+++ utils/tweet.py
@@ -0,0 +1,31 @@
+import os
+import tweepy
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# TWITTER API CREDENTIALS 
+consumerKey = os.environ.get("CONSUMER_KEY")
+consumerSecret = os.environ.get("CONSUMER_SECRET")
+accessToken = os.environ.get("ACCESS_TOKEN")
+accessTokenSecret = os.environ.get("ACCESS_TOKEN_SECRET")
+
+auth = tweepy.OAuth1UserHandler(
+    consumerKey,
+    consumerSecret,
+    accessToken,
+    accessTokenSecret
+)
+client = tweepy.API(auth)
+
+# The app and the corresponding credentials must have the Write permission
+def tweet(text, media = None):
+    if media == None:
+        response = client.update_status(
+            text = text
+        )
+        print(f"https://twitter.com/user/status/{response.data['id']}")
+    else:
+        media_file = client.media_upload(filename = media)
+        response = client.update_status(status = text, media_ids = [media_file.media_id_string])
+        print('Tweeted.................................')
