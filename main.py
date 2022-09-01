dee8c55f88c9561856745447ec395de03c24896a
engr-solution <dah.solu.tion7@gmail.com>
2022-09-01 01:29:15 +0100

    add chromedriver autoinstaller lib

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from keep_alive import alive
 from utils.scrapper import blizzard_forum_scrapper
+import chromedriver_autoinstaller
 
-# CONFIG FOR DEVELOPMENT ENV
-# PATH = "C:\Program Files (x86)\chromedriver.exe"    # Replace with your chromedriver path 
+chromedriver_autoinstaller.install()
 
 # CONFIG FOR PRODUCTION ENV
 chrome_options = webdriver.ChromeOptions()
@@ -17,10 +18,10 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
+alive()
 
 while True:
-    # driver = webdriver.Chrome(PATH)               // CONFIG FOR DEVELOPMENT ENV 
-    driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)      
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)            
    
     print('Scraping blizzard forum site...........................')
     blizzard_forum_scrapper(driver, WebDriverWait, By, EC)
