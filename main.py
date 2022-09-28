88d464eeab4b96bf14ef299c1143583bcf6d4f6d
engr-solution <dah.solu.tion7@gmail.com>
2022-09-28 07:04:24 +0100

    add selenium options to run on linux vps

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,23 +10,28 @@ from selenium.webdriver.support import expected_conditions as EC
 from utils.scrapper import blizzard_forum_scrapper
 import chromedriver_autoinstaller
 
-chromedriver_autoinstaller.install()
+#chromedriver_autoinstaller.install()
 
 # CONFIG FOR PRODUCTION ENV
 chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option('useAutomationExtension', False)
 
-# alive()
+#alive()
 
 while True:
-    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)            
+    #driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+    driver = webdriver.Chrome(service=Service(chromedriver_autoinstaller.install()), options=chrome_options)
+       
    
     print('Scraping blizzard forum site...........................')
     blizzard_forum_scrapper(driver, WebDriverWait, By, EC)
 
-    # Sleep for 1 min then continue
-    time.sleep(60)
+    # Sleep for 10secs then continue
+    time.sleep(10)
 
